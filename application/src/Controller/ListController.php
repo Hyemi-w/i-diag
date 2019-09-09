9c8d85d8f2481b28d6793270d20fd9ac2707c9ed
hyemi-w <elodie.billon1995@outlook.fr>
2019-09-09 08:44:31 +0200

    route changed for the list page from /list to /

--- application/src/Controller/ListController.php
+++ application/src/Controller/ListController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ListController extends AbstractController
 {
     /**
-     * @Route("/liste", name="list")
+     * @Route("/", name="list")
      */
     public function index()
     {
