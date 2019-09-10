41a5e6a7838d9e769f07597071175328dd701357
hyemi-w <elodie.billon1995@outlook.fr>
2019-09-10 15:20:42 +0200

    form started

--- application/src/Controller/ScoffTestController.php
+++ application/src/Controller/ScoffTestController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\ScoffTest;
+use App\Form\ScoffTestType;
 use App\Repository\ScoffTestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +15,23 @@ class ScoffTestController extends AbstractController
     /**
      * @Route("/scoff-test", name="scoff_test")
      */
-    public function index(ScoffTestRepository $scoffTestRepository) : Response
+//    public function index(ScoffTestRepository $scoffTestRepository) : Response
+//    {
+//        return $this->render('scoff_test/index.html.twig', [
+//            'scoff_tests'=> $scoffTestRepository->findAll()
+//        ]);
+//    }
+    public function contact(Request $request, ScoffTestRepository $scoffTestRepository) : Response
     {
+        $scoff = new ScoffTest();
+        $form = $this->createForm(ScoffTestType::class, $scoff);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+        }
         return $this->render('scoff_test/index.html.twig', [
-            'scoff_tests'=> $scoffTestRepository->findAll()
+            'scoff_tests' => $scoffTestRepository->findAll(),
+            'form' => $form->createView()
         ]);
     }
 }
