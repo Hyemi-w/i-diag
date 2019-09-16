5e618e8a20468d34f4c705732463726971d6dfd4
hyemi-w <elodie.billon1995@outlook.fr>
2019-09-16 14:08:54 +0200

    bref

--- application/src/Form/ScoffTestType.php
+++ application/src/Form/ScoffTestType.php
@@ -4,16 +4,17 @@
 namespace App\Form;
 
 use App\Entity\ScoffTest;
+use App\Repository\ScoffTestRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ScoffTestType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options, ScoffTestRepository $scoffTestRepository)
     {
-        $scoffTest = new ScoffTest();
-        $questions = $scoffTest->getQuestion();
+        $questions = $scoffTestRepository->findBy([]);
+        var_dump($questions);
         foreach ($question as $questions) {
             $builder->add('question', ChoiceType::class, ['choices' => [
             'Oui' => '1',
