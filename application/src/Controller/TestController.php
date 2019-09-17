7dd5748cd5f19c967645a4e51afe46c091bfc6c6
hyemi-w <elodie.billon1995@outlook.fr>
2019-09-17 11:31:41 +0200

    fetch data corrected

--- application/src/Controller/TestController.php
+++ application/src/Controller/TestController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Test;
 use App\Form\TestType;
 use App\Repository\TestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
@@ -12,12 +14,15 @@ class TestController extends AbstractController
     /**
      * @Route("/test", name="test")
      */
-    public function index(TestRepository $testRepository)
+    public function index(TestRepository $testRepository) : Response
     {
-        $form = $this->createForm(
-            TestType::class,
-            null
-        );
+//        $test = new Test();
+        $form = $this->createForm(TestType::class, null);
+
+//        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $test = $form->getData();
+//        }
         return $this->render('test/index.html.twig', [
             'tests' => $testRepository->findAll(),
             'form' => $form->createView(),
--- application/src/Form/TestType.php
+++ application/src/Form/TestType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\Test;
 use App\Repository\TestRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +13,15 @@ class TestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        foreach ($test as $tests)
-        $builder->add('question', ChoiceType::class, ['choices' => [
-        'Oui' => '1',
-        'Non' => '2'],
-        'multiple' => false, 'expanded' => true]);
+        $builder
+            ->add('question', EntityType::class, [
+                'class' => Test::class,
+                'multiple' => true,
+                'expanded' => true,
+                'query_builder' => function (TestRepository $testRepository) {
+                    return $testRepository->createQueryBuilder('t')
+                        ->orderBy('t.id', 'ASC');
+            }]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
