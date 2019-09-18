c03056616de060bf9608fa666b120cf6281b140f
hyemi-w <elodie.billon1995@outlook.fr>
2019-09-18 11:30:04 +0200

    reorganisation code

--- application/src/Form/ScoffTestType.php
+++ application/src/Form/ScoffTestType.php
@@ -7,22 +7,22 @@ use App\Entity\ScoffTest;
 use App\Repository\ScoffTestRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ScoffTestType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-            $builder
-                ->add('question', EntityType::class, ['choices' => [
-                    'Oui' => '1',
-                    'Non' => '2'],
-                    'multiple' => false, 'expanded' => true,
-                    'class' => ScoffTest::class,
-                    'query_builder' => function (ScoffTestRepository $scoffTestRepository) {
-                    return $scoffTestRepository->createQueryBuilder('s')
-                        ->orderBy('t.id', 'ASC');
-                    }]);
+        $builder
+            ->add('question', EntityType::class, ['choices' => [
+                'Oui' => '1',
+                'Non' => '2'],
+                'multiple' => false, 'expanded' => true,
+                'class' => ScoffTest::class,
+                'query_builder' => function (ScoffTestRepository $scoffTestRepository) {
+                return $scoffTestRepository->createQueryBuilder('s')
+                    ->orderBy('t.id', 'ASC');
+                    }]
+                );
     }
 }
